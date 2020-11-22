e8c1d572ab380c2d3ad7187ee336cf12740554b9
ialtafshaikh <altaf.shaikh@contentstack.com>
2020-11-22 15:23:38 +0530

    card layout fixed and flex applied to employee container containing cards

--- src/script.js
+++ src/script.js
@@ -193,11 +193,16 @@ const createCard = (employee) => {
   const div = document.createElement("div");
   div.classList.add("card");
 
+  const cardWrapper = document.createElement("div");
+  cardWrapper.classList.add("card-wrapper");
+
   const profileImg = createProfileImg(employee.picUrl, employee.altDescp);
   const cardContent = createCardContent(employee);
 
-  div.appendChild(profileImg);
-  div.appendChild(cardContent);
+  cardWrapper.appendChild(profileImg);
+  cardWrapper.appendChild(cardContent);
+
+  div.appendChild(cardWrapper);
 
   return div;
 };
