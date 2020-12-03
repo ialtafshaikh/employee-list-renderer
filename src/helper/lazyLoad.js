399383b8163a9a6ca76dc531d4e618e9e7ad5601
ialtafshaikh <altaf.shaikh@contentstack.com>
2020-12-03 15:51:40 +0530

    spinner added in lazy loading

--- src/helper/lazyLoad.js
+++ src/helper/lazyLoad.js
@@ -5,16 +5,25 @@ let start = 8;
 let amountOfCardFetch = 4;
 let end = start + amountOfCardFetch;
 
-export const lazyLoad = () => {
-  Trainees.then((employees) => {
-    if (!(start >= employees.length)) {
-      employees.slice(start, end).forEach((employee) => {
-        renderEmployee(employee);
-      });
-      start += amountOfCardFetch;
-      end = start + amountOfCardFetch;
-    } else {
-      document.getElementById("load").textContent = "no more content to load";
-    }
+export const lazyLoad = (event) => {
+  event.target.style.display = "none";
+
+  const loader = document.getElementById("loader");
+  loader.style.display = "block";
+  loader.addEventListener("animationend", () => {
+    loader.style.display = "none";
+    event.target.style.display = "block";
+    Trainees.then((employees) => {
+      if (!(start >= employees.length)) {
+        employees.slice(start, end).forEach((employee) => {
+          renderEmployee(employee);
+        });
+        start += amountOfCardFetch;
+        end = start + amountOfCardFetch;
+      } else {
+        document.getElementById("load").textContent = "no more content to load";
+        document.getElementById("load").disabled = true;
+      }
+    });
   });
 };
